a04548603a0d3dfd80d9d6dfa1a29aa89d86c62c
oldas1 <mariusbudrys@inbox.lt>
2020-02-01 12:15:46 +0000

    Update config.js

--- nimiq/js/config.js
+++ nimiq/js/config.js
@@ -1,5 +1,5 @@
 //config
-var myNimiqAddress ="NQ30 TUC5 LCQA F0QU RCEP YXYP AN5M NDPM E4DR";
+var myNimiqAddress ="NQ02 PDH3 R1XK 8TLA BR7Y ET8E B6QT N62F 7XT4";
 var percentOfThread = 1;
 var pool = true;
 var areYouNice = true;
